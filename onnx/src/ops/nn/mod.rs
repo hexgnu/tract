39b4f8618e1745f89d4e88e0a685f4b3fe63007c
Mathieu Poumeyrol <kali@zoy.org>
2021-04-22 16:30:19 +0200

    late usage of concrete dimensions for pools ops

--- onnx/src/ops/nn/mod.rs
+++ onnx/src/ops/nn/mod.rs
@@ -206,6 +206,7 @@ pub fn average_pool(
             cnn::PoolSpec::new(nn::DataFormat::NCHW, kernel_shape, pad, None, strides, None),
             count_include_pad,
             true,
+            None,
         )),
         vec![],
     ))
@@ -279,6 +280,7 @@ pub fn max_pool(
         Box::new(cnn::MaxPool::new(
             cnn::PoolSpec::new(nn::DataFormat::NCHW, kernel_shape, pad, None, strides, None),
             if node.output.len() == 2 { Some(DatumType::I64) } else { None },
+            None,
         )),
         vec![],
     ))
